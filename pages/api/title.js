097384a5e45339753c345c85bf56e132006a8a16
AbhishekChugh <achugh89@gmail.com>
2023-09-24 16:55:18 +0530

    Update

--- pages/api/title.js
+++ pages/api/title.js
@@ -1,5 +1,8 @@
 // Returns the title of the application in json format.
 // { title: "Application" }
 export default function handler(req, res) {
-  res.status(200).json({ title: "Application" });
+  const lang = req.query?.lang || "EN";
+  if (lang === "EN") res.status(200).json({ title: "Website" });
+  if (lang === "FR") res.status(200).json({ title: "site web" });
+  if (lang === "DE") res.status(200).json({ title: "Webseite" });
 }
