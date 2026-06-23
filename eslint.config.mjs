8ea48480cbddbcbe861abaca024ffcc1f51e924a
abchugh <achugh89@gmail.com>
2026-06-23 16:07:34 +0530

    Upgrade deps

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextCoreWebVitals,
+  {
+    settings: {
+      react: {
+        version: "19",
+      },
+    },
+  },
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
